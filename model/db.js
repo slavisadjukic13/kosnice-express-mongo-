5c2552abddd95752c18111c55bf47771be85d800
slavisadjukic13 <slavisadjukic13@gmail.com>
2016-11-04 16:28:26 +0100

    switched to local db for offline work

--- model/db.js
+++ model/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://slavisauser:slavisapass@ds143737.mlab.com:43737/slavisa');
-// mongoose.connect('mongodb://localhost:27017/mongo');
+// mongoose.connect('mongodb://slavisauser:slavisapass@ds143737.mlab.com:43737/slavisa');
+mongoose.connect('mongodb://localhost:27017/mongo');
